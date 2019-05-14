249c30e00cda82c4079a22689fb2ff21452a7791
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2019-05-14 14:38:47 +0530

    Changed SQL URI

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,8 @@ import os
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rulsyzlidrkofp:4492814d38bcfc278e6f25bfcc7fe43f15671737cb0d6477db5ccef9d5ad57b1@ec2-54-163-226-238.compute-1.amazonaws.com:5432/dblp20cuc3ltv1'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 db = SQLAlchemy(app)
