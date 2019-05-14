816a823410cca15dde71b23bd74001653b4b7170
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2019-05-14 15:38:55 +0530

    Tried some db changes and created tables

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy.orm import scoped_session, sessionmaker
+#from sqlalchemy import create_engine
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
@@ -8,6 +10,7 @@ import os
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
+#engine = create_engine('postgres://rulsyzlidrkofp:4492814d38bcfc278e6f25bfcc7fe43f15671737cb0d6477db5ccef9d5ad57b1@ec2-54-163-226-238.compute-1.amazonaws.com:5432/dblp20cuc3ltv1')
 app.config['SECRET_KEY'] = SECRET_KEY
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rulsyzlidrkofp:4492814d38bcfc278e6f25bfcc7fe43f15671737cb0d6477db5ccef9d5ad57b1@ec2-54-163-226-238.compute-1.amazonaws.com:5432/dblp20cuc3ltv1'
